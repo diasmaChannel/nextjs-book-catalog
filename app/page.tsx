a2fcc53536eae0eb3e90283c2f511bb22df5dc51
Ciel <danagames30@gmail.com>
2023-06-23 20:18:29 +0700

    Update Landing Page, Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,45 @@
 import Navbar from '@/components/nav/navbar'
-import Image from 'next/image'
+import BookCard from '@/components/partials/bookCard'
+import Footer from '@/components/partials/footer'
+import {BsSearch} from 'react-icons/bs'
 
 export default function Home() {
   return (
     <>
       <Navbar/>
+      
+      <section className='mt-10'>
+        <div className='text-center text-4xl font-bold my-8'>
+          <h1>Our Best Books</h1>
+        </div>
+        <div className='flex flex-wrap gap-4 justify-evenly my-5'>
+          <BookCard/>
+          <BookCard/>
+          <BookCard/>
+          <BookCard/>
+        </div>
+      </section>
+
+      <section className='flex justify-center my-20'>
+        <div className='bg-slate-100 p-32 w-[80%] border rounded-3xl'>
+          <div className='text-center text-4xl font-bold my-8'>
+            <h1>Find Your Book</h1>
+          </div>
+          <div className='flex justify-center'>
+            <div className="hidden md:block relative w-1/2 mt-3">
+                <input type="text" className="block w-full h-full text-xl rounded-md border-gray-300 p-5" placeholder="Search" />
+                <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                    <BsSearch size={20}/>
+                </div>
+            </div>
+          </div>
+        </div>
+        
+      </section>
+
+
+      <Footer/>
+    
     </>
   )
 }
--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -16,14 +16,6 @@ export default function Navbar() {
             <h1 className="md:text-2xl text-xl text-white">Book Catalog</h1>
         </div>
 
-        <div className="hidden md:relative h-1/2 mt-3">
-            <input type="text" className="block w-full h-full rounded-md border-gray-300 p-2" placeholder="Search" />
-            <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <BsSearch size={20}/>
-            </div>
-            
-        </div>
-
         <div className="md:hidden flex items-center ml-2">
             <button className="border border-gray-300 rounded-md p-2.5 hover:bg-gray-500" onClick={toggleMenu}>
                 <FiMenu className="text-2xl text-white"/>
@@ -35,16 +27,19 @@ export default function Navbar() {
         <div className="hidden md:flex items-center" id="links">
             <ul className="flex justify-evenly gap-4 text-white">
                 <li className="hover:text-gray-300">
-                    <a href="">Link 1</a>
+                    <a href="">Home</a>
                 </li>
                 <li className="hover:text-gray-300">
-                    <a href="">Link 2</a>
+                    <a href="">About</a>
+                </li>
+                <li className="hover:text-gray-300">
+                    <a href="">Categories</a>
                 </li>
             </ul>
             
 
             {/* Auth */}
-            <div className="ml-10">
+            <div className="ml-20">
                 <BsPersonCircle size={30} color="#ffffff"/>
             </div>
         </div>
